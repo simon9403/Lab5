ebe2a6f49cf0bb5a9257d2cf084e5131e4492ee4
simon9403 <crossgw94@gmail.com>
2016-10-03 13:58:41 +0200

    Test needs to be studied

--- tests/testthat/test_API_resp.R
+++ tests/testthat/test_API_resp.R
@@ -3,7 +3,7 @@ library(httr)
 context("API response")
 
 test_that("API returned json", {
-  expect_equal(http_type((google_api("address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"))$response), "application/json")
-  expect_equal(class(google_api("address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")),"google_api_class")
-  expect_equal((google_api("address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"))$response$status_code ,200)
+  expect_equal(http_type(google_api("1600+Amphitheatre+Parkway,+Mountain+View,+CA")), "application/json")
+  expect_equal(class(google_api("1600+Amphitheatre+Parkway,+Mountain+View,+CA")),"google_api_class")
+  expect_equal((google_api("1600+Amphitheatre+Parkway,+Mountain+View,+CA"))$response$status_code ,200)
   })
